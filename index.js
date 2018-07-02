a652adbeee1a26e573f5248eb4f10daa1bc712bd
mdmills1990@gmail.com <mdmills1990@gmail.com>
2018-07-02 22:51:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ function happyHolidays(){
 function happyHolidaysTo(name){
   return 'Happy holidays, ${name}!'
 }
-function happyHolidayTo()
+function happyHolidayTo(holiday,name){
+  return 'Happy ${'
+}
